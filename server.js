426ad627565336569cec7e793e0b35848cfe7992
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-02-11 12:34:48 -0800

    added logger middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -10,6 +11,20 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+ const {method, url } = req;
+//  function timeStamp(){
+//    const d = new Date();
+//    const year = d.getFullYear;
+//    const month = d.getMonth()+1
+//    const date = d.getDate();
+//    return `${month}/${date}/${year}`
+//  }
+
+ 
+ 
+ console.log(method, url, new Date())
+ next()
+}
 
 module.exports = server;
